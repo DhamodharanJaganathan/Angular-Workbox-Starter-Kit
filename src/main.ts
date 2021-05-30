9da5d393dd043630eb28a160aaba475a54a987e8
DhamodharanJaganathan <dhamodharan.jaganathan@gmail.com>
2021-05-30 13:55:44 +0530

    event listner for SW

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,45 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { Workbox } from 'workbox-window';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { Workbox } from "workbox-window";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-.then( _=> loadServiceWorker())
-.catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then((_) => loadServiceWorker())
+  .catch((err) => console.error(err));
 
 function loadServiceWorker() {
-  if ('serviceWorker' in navigator) {
-   const wb = new Workbox('service-worker.js');
-   wb.register();
+  if ("serviceWorker" in navigator) {
+    const wb = new Workbox("service-worker.js");
+    wb.addEventListener("activated", async (event) => {
+      if (!event.isUpdate) {
+        console.log("Service worker activated for the first time!");
+      } else {
+        console.log("Service worker activated!");
+      }
+    });
+
+    wb.addEventListener("waiting", (event) => {
+      console.log(
+        `A new service worker has installed, but it can't activate` +
+          `until all tabs running the current version have fully unloaded.`
+      );
+    });
+    
+    wb.addEventListener("installed", (event) => {
+      if (!event.isUpdate) {
+        console.log("Service worker installed for the first time");
+      } else {
+        console.log("Service worker installed");
+      }
+    });
+    
+    wb.register();
   }
- }
+}
